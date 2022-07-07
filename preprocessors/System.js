fe91a88d86bd5d46097b45ada8eb07ea914d2be5
Garrett Sutula <garrettsutula@gmail.com>
2022-07-07 11:46:01 -0400

    Update example project with bugfixes

--- preprocessors/System.js
+++ preprocessors/System.js
@@ -1,6 +1,5 @@
 module.exports  = async (schema) => {
-  const { components = [] } = schema;
-  Object.keys(components).forEach((componentKey) => {
+  Object.keys(schema.components).forEach((componentKey) => {
     const component = schema.components[componentKey];
     // Adds a back-reference to each component so system name is easily accessible from {component}.parent.name
     component.parent = {$ref: "#"};
