8ece357a368d858a46efab4e2f79cc5387254425
Garrett Sutula <garrett.sutula@gmail.com>
2022-07-23 14:41:39 -0400

    Update dependencies & project template

--- preprocessors/System.js
+++ preprocessors/System.js
@@ -1,11 +1,24 @@
-module.exports  = async (schema) => {
-  Object.keys(schema.components).forEach((componentKey) => {
+const nameToId = require('./lib/nameToId');
+
+function preprocessingFn(schema) {
+  const { components = [] } = schema;
+  Object.keys(components).forEach((componentKey) => {
     const component = schema.components[componentKey];
     // Adds a back-reference to each component so system name is easily accessible from {component}.parent.name
-    component.parent = {$ref: "#"};
-    component.id = component.name; // TODO: sanitizer util
+    component.parent = { $ref: '#' };
+    component.id = nameToId(component.name);
+    component.modelType = 'component';
   });
-  // Adds system id
-  schema.id = schema.name; // TODO: sanitizer util
+  schema.id = nameToId(schema.name);
+  schema.modelType = 'system';
   return schema;
 }
+
+function postprocessingFn(schema) {
+  return schema;
+}
+
+module.exports = {
+  preprocessingFn,
+  postprocessingFn,
+};
