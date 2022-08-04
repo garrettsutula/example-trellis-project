fe709b73bee175afef16b2c4bc6b46cb1a2d8776
Garrett Sutula <garrettsutula@gmail.com>
2022-08-03 20:02:49 -0400

    Switch to c4 example

--- preprocessors/System.js
+++ preprocessors/System.js
@@ -1,20 +1,24 @@
-function preprocessFn(schema) {
-  Object.keys(schema.components).forEach((componentKey) => {
+const nameToId = require('./lib/nameToId');
+
+function preprocessingFn(schema) {
+  const { components = [] } = schema;
+  Object.keys(components).forEach((componentKey) => {
     const component = schema.components[componentKey];
     // Adds a back-reference to each component so system name is easily accessible from {component}.parent.name
     component.parent = { $ref: '#' };
-    component.id = component.name; // TODO: sanitizer util
+    component.id = nameToId(component.name);
+    component.modelType = 'component';
   });
-  // Adds system id
-  schema.id = schema.name; // TODO: sanitizer util
+  schema.id = nameToId(schema.name);
+  schema.modelType = 'system';
   return schema;
 }
 
-function postprocessFn(schema) {
+function postprocessingFn(schema) {
   return schema;
 }
 
 module.exports = {
-  preprocessFn,
-  postprocessFn,
+  preprocessingFn,
+  postprocessingFn,
 };
